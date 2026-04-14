eec342899002e1faf2866577cb096c84d7928e19
Saswata Adhya <saswataadhya@gmail.com>
2026-04-14 10:49:51 +0530

    Demo on Optional

---
+++ src/main/java/java8/StreamsDemo.java
@@ -0,0 +1,19 @@
+package java8;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamsDemo {
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+
+        List<Integer> result = numbers.stream()
+                .filter(n -> n % 2 == 0)    // Filter even numbers
+                .map(n -> n * n)            // Square each number
+                .collect(Collectors.toList());     // Collect as a list
+        //      .toList();                         // Can also be replaced with
+
+        System.out.println("Squares of even numbers: " + result);
+    }
+}
